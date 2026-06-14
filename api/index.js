e2cc1ae0992c194e598ecdc69ffbe551bb9bf6be
sufiyan <sufiyan.codyvent@gmail.com>
2026-06-14 07:54:12 +0500

    Route Vercel API traffic to Express

--- api/index.js
+++ api/index.js
@@ -5,9 +5,21 @@ async function ensureReady() {
   assertEnv()
 }
 
+function restoreApiPath(req) {
+  const url = new URL(req.url, 'http://vercel.local')
+  const routedPath = url.searchParams.get('path')
+
+  if (!routedPath) return
+
+  url.searchParams.delete('path')
+  const query = url.searchParams.toString()
+  req.url = `/api/${routedPath}${query ? `?${query}` : ''}`
+}
+
 export default async function handler(req, res) {
   try {
     await ensureReady()
+    restoreApiPath(req)
     return app(req, res)
   } catch (error) {
     console.error('[api:init]', error)
